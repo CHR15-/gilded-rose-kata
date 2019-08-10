cba8b0837a04a297510bad5354b7c3a9a7c19e80
Chris Evans <mail@chr1s.it>
2019-08-10 23:38:08 +0100

    backstage class

---
+++ rewrite/src/items/BackstagePasses.js
@@ -0,0 +1,31 @@
+import RegularItem from "./RegularItem";
+import { DEGRADATION_RATE, MAX_QUALITY, MIN_QUALITY } from '../constants';
+
+class BackstagePasses extends RegularItem {
+  constructor(name, sellIn, quality){
+    super(name, sellIn, quality, false)
+  }
+
+  /* Backstage passes, like aged brie, increases in Quality as it's SellIn value approaches;
+    Quality increases by 2 when there are 10 days or less and by 3 when there are
+    5 days or less but Quality drops to 0 after the concert
+  */
+  updateQuality() {
+    // bail out if concert is finished
+    if (this.sell_in < 0) {
+      return MIN_QUALITY;
+    }
+
+    if (this.quality < MAX_QUALITY) {
+      if (this.sell_in <= 5) {
+        return this.quality <= 47 ? this.quality += 3 : MAX_QUALITY;
+      } else if (this.sell_in <= 10) {
+        return this.quality <= 48 ? this.quality += 2 : MAX_QUALITY;
+      }
+      return this.quality += DEGRADATION_RATE;
+    }
+  }
+
+}
+
+export default BackstagePasses;
