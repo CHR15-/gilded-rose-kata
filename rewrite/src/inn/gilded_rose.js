d084ffba213c70bc30bac4257395c900000eaff1
Chris Evans <mail@chr1s.it>
2019-08-10 23:59:59 +0100

    store wrapper and fix classes to conform to it

--- rewrite/src/inn/gilded_rose.js
+++ rewrite/src/inn/gilded_rose.js
@@ -1,6 +1,23 @@
 let items = [];
 
+const itemWrapper = (item) => {
+  switch(item.name) {
+    case 'Aged Brie':
+      return new AgedBrie(item);
+    case 'Sulfuras, Hand of Ragnaros':
+      return new Legendary(item);
+    case 'Backstage passes to a TAFKAL80ETC concert':
+      return new BackstagePasses(item);
+    default:
+      return item.includes('Conjured') ? new Conjured(item) : new RegularItem(item);
+  }
+};
+
 export const update_quality = (items) => {
+  items = items.map(function(item) {
+    return itemWrapper(item);
+  });
+
   items.forEach(item => {
     item.updateItem();
   });
--- rewrite/src/items/Legendary.js
+++ rewrite/src/items/Legendary.js
@@ -2,8 +2,8 @@ import RegularItem from "./RegularItem";
 import { LEGENDARY_QUALITY } from '../constants';
 
 class Legendary extends RegularItem {
-  constructor(name, sellIn, quality) {
-    super(name, sellIn, LEGENDARY_QUALITY)
+  constructor(item) {
+    super(item)
   }
 
   // a legendary item's quality is 80 and it never alters
