0d3bcc0d768bcf96290f04f14955c4bb9ddb20bd
Chris Evans <mail@chr1s.it>
2019-08-10 23:34:58 +0100

    extract item to class, add regularitem class

---
+++ rewrite/src/items/RegularItem.js
@@ -0,0 +1,23 @@
+import Item from './Item';
+import { DEGRADATION_RATE, MAX_QUALITY, MIN_QUALITY } from '../constants';
+
+class RegularItem extends Item {
+  constructor(name, sellIn, quality){
+    super(name, sellIn, quality)
+  }
+
+  updateQuality() {
+    var eopQuality = this.sell_in < 0 ? this.quality -= DEGRADATION_RATE * 2 : this.quality -= DEGRADATION_RATE;
+    if (this.quality < 0) return MIN_QUALITY;
+    if (this.quality > 50) return MAX_QUALITY;
+    return eopQuality;
+  }
+
+  updateItem() {
+    this.quality = this.updateQuality();
+    this.sell_in -= 1;
+  }
+
+}
+
+export default RegularItem;
